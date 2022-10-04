51d6fa584303640ef53ef269f1cae3cbd08ebdcc
Altify <Altify@mail.com>
2022-10-04 15:12:39 -0700

    Update deepcap.js

--- deepcap.js
+++ deepcap.js
@@ -1,7 +1,6 @@
 
 function LoadDoc() {
         var client = new XMLHttpRequest();
-        client.open('GET', 'ajax.txt');
           client.onload = function() {
             document.getElementById("test").innerHTML = this.responseText;
             }
