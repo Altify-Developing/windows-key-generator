dccfb03946037e2fa5d13ca5d9ceb92c9073f246
Altify <Altify@mail.com>
2022-10-04 15:04:40 -0700

    Update deepcap.js

--- deepcap.js
+++ deepcap.js
@@ -1,15 +1,15 @@
 
 function LoadDoc() {
         var client = new XMLHttpRequest();
-        client.open('GET', '/resources/states.txt');
+        client.open('GET', 'resources/states.txt');
         client.onreadystatechange = function()
         {
             if( client.responseText != '' )
             {
                 var txt = client.responseText.split("\n");
-                let state = Math.floor(Math.random() * 1) + 1960;
-                let combo = ('state: '+txt[state]);
-                document.getElementById("test").innerHTML = (combo);
+                let state = Math.floor(Math.random() * 1960) + 1;
+                let combo = (txt[state]);
+                document.getElementById("test").innerHTML = combo;
             }
         }
 }
